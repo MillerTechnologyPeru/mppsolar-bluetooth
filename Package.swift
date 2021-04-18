a26ae5b966adac522008ef5b46aed2b50d081af1
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-04-18 18:04:51 -0500

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.1
 import PackageDescription
 
-let package = Package(
+var package = Package(
     name: "mppsolar-bluetooth",
+    platforms: [
+        .macOS(.v10_12)
+    ],
     products: [
         /*
         .executable(
-            name: "mppsolar-bluetooth-deamon",
+            name: "mppsolar-bluetooth",
             targets: ["MPPSolarBluetoothServer"]
         ),*/
         .library(
@@ -40,16 +41,33 @@ let package = Package(
         .package(
             url: "https://github.com/PureSwift/BluetoothDarwin.git",
             .branch("master")
-        )
+        ),
+        .package(
+            url: "https://github.com/apple/swift-crypto.git",
+            .upToNextMinor(from: "1.1.6")
+        ),
+        .package(
+            url: "https://github.com/PureSwift/TLVCoding.git",
+            .branch("master")
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MPPSolarBluetooth",
-            dependencies: []),
+            dependencies: [
+                "MPPSolar",
+                "Bluetooth",
+                "GATT",
+                "TLVCoding"
+            ]
+        ),
         .testTarget(
             name: "MPPSolarBluetoothTests",
-            dependencies: ["MPPSolarBluetooth"]),
+            dependencies: ["MPPSolarBluetooth"]
+        ),
     ]
 )
+
+#if os(Linux)
+package.targets[0].dependencies.append("Crypto")
+#endif
